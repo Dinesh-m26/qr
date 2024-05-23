eb762e37544adff8194a9ea0525cf18a022def2b
dinesh <dineshubli@gmail.com>
2024-05-23 21:17:56 +0530

    add as app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Qrcode from './Components/Qrcode'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Qrcode />
+   <App/>
   </React.StrictMode>,
 )
